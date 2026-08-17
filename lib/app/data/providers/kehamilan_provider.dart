192897b68d90fb160155d536c0f310e1b0f25b62
maouzal-pandu <maozalpandu123@gmail.com>
2026-08-18 03:54:58 +0700

    feat: Add anc apis

--- lib/app/data/providers/kehamilan_provider.dart
+++ lib/app/data/providers/kehamilan_provider.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:monitoring_bumil_application/app/data/models/schedule_anc.dart';
 
 class KehamilanProvider {
   final _baseUrl = const String.fromEnvironment(
@@ -37,6 +38,73 @@ class KehamilanProvider {
       if (response.statusCode != 200) {
         throw Exception(data["detail"] ?? {"detail": "Error"});
       }
+
+      return data;
+    } catch (e) {
+      throw Exception("Error: $e");
+    }
+  }
+
+  Future<Map<String, dynamic>> setAntenatalCareSchedule({
+    required int kehamilanId,
+    required DateTime tanggalJadwal,
+    required String catatan,
+  }) async {
+    final url = Uri.parse("$_baseUrl/set-jadwal-anc");
+
+    try {
+      final response = await http.post(
+        url,
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode({
+          "kehamilan_id": kehamilanId,
+          "tanggal_jadwal": tanggalJadwal,
+          "catatan": catatan,
+        }),
+      );
+
+      final data = jsonDecode(response.body);
+
+      return data;
+    } catch (e) {
+      throw Exception("Error: $e");
+    }
+  }
+
+  Future<List<JadwalAnc>> getAntenatalCareSchedule({
+    required int kehamilanId,
+  }) async {
+    final url = Uri.parse("$_baseUrl/jadwal-anc/$kehamilanId");
+
+    try {
+      final response = await http.get(url);
+
+      final data = jsonDecode(response.body);
+
+      if (response.statusCode != 200) {
+        throw Exception(data["detail"] ?? "Gagal memuat jadwal anc");
+      }
+
+      return (data as List).map((e) => JadwalAnc.fromJson(e)).toList();
+    } catch (e) {
+      throw Exception("Error: $e");
+    }
+  }
+
+  Future<Map<String, dynamic>> setAntenatalCareScheduleStatus({
+    required int scheduleId,
+    required String status,
+  }) async {
+    final url = Uri.parse("$_baseUrl/change-jadwal-anc-status");
+
+    try {
+      final response = await http.post(
+        url,
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode({"schedule_id": scheduleId, "status": status}),
+      );
+
+      final data = jsonDecode(response.body);
 
       return data;
     } catch (e) {
