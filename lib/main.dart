ed8d0eb0d824181df60d68c72ba3fd82dc3d44a9
maouzal-pandu <maozalpandu123@gmail.com>
2026-08-18 19:14:58 +0700

    feat: Add fonts

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:monitoring_bumil_application/app/core/bindings/initial_binding.dart';
 import 'package:monitoring_bumil_application/app/core/theme/app_theme.dart';
 
 import 'app/routes/app_pages.dart';
@@ -10,8 +12,11 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await GetStorage.init();
+  initializeDateFormatting("id_ID");
+
   runApp(
     GetMaterialApp(
+      initialBinding: InitialBinding(),
       title: "Monitoring Bumil App",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
