73f9b4b0720150ac69cb861c95b2f2fadeedba6b
maouzal-pandu <maozalpandu123@gmail.com>
2026-08-18 19:18:00 +0700

    fix: bind schedule and history controller

--- lib/app/modules/bumil-home/bindings/bumil_home_binding.dart
+++ lib/app/modules/bumil-home/bindings/bumil_home_binding.dart
@@ -1,12 +1,14 @@
 import 'package:get/get.dart';
+import 'package:monitoring_bumil_application/app/modules/bumil-history/controllers/bumil_history_controller.dart';
+import 'package:monitoring_bumil_application/app/modules/bumil-schedule/controllers/bumil_schedule_controller.dart';
 
 import '../controllers/bumil_home_controller.dart';
 
 class BumilHomeBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<BumilHomeController>(
-      () => BumilHomeController(),
-    );
+    Get.lazyPut<BumilHomeController>(() => BumilHomeController());
+    Get.lazyPut<BumilScheduleController>(() => BumilScheduleController());
+    Get.lazyPut<BumilHistoryController>(() => BumilHistoryController());
   }
 }
