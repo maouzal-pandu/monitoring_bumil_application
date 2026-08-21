5baff33153e7399eaa640bce835ec4e883f8794b
maouzal-pandu <maozalpandu123@gmail.com>
2026-08-21 23:24:51 +0700

    feat: add logout

--- lib/app/modules/settings/controllers/settings_controller.dart
+++ lib/app/modules/settings/controllers/settings_controller.dart
@@ -1,8 +1,15 @@
 import 'package:get/get.dart';
 import 'package:monitoring_bumil_application/app/data/providers/auth_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsController extends GetxController {
   final AuthProvider _authService = Get.find<AuthProvider>();
 
-  RxString get currentRole => _authService.role;
+  Rx<RoleUser> get currentRole => _authService.role;
+
+  Future<void> logout() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.clear();
+    Get.offAllNamed("/login");
+  }
 }
--- lib/app/modules/settings/views/settings_view.dart
+++ lib/app/modules/settings/views/settings_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:monitoring_bumil_application/app/core/theme/app_colors.dart';
+import 'package:monitoring_bumil_application/app/data/providers/auth_provider.dart';
 
 import '../controllers/settings_controller.dart';
 
@@ -12,12 +14,55 @@ class SettingsView extends GetView<SettingsController> {
       appBar: AppBar(title: const Text('SettingsView'), centerTitle: true),
       body: Center(
         child: Obx(
-          () => Text(
-            controller.currentRole.value,
-            style: TextStyle(fontSize: 20),
+          () => SingleChildScrollView(
+            child: Column(
+              children: [
+                if (controller.currentRole.value == RoleUser.bumil)
+                  _bumilSettings(),
+                if (controller.currentRole.value == RoleUser.bidan)
+                  _bidanSettings(),
+                if (controller.currentRole.value == RoleUser.admin)
+                  _adminSettings(),
+
+                // logout button
+                ListTile(
+                  leading: Icon(Icons.logout_rounded, color: AppColors.error),
+                  title: Text(
+                    'Keluar',
+                    style: TextStyle(
+                      color: AppColors.error,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  onTap: () {
+                    Get.defaultDialog(
+                      title: 'Konfirmasi',
+                      middleText: 'Apakah kamu yakin ingin keluar?',
+                      textCancel: 'Batal',
+                      textConfirm: 'Keluar',
+                      confirmTextColor: Colors.white,
+                      buttonColor: AppColors.error,
+                      onConfirm: () => controller.logout(),
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  Widget _bumilSettings() {
+    return Column(children: [const Text("Bumil Setting")]);
+  }
+
+  Widget _bidanSettings() {
+    return Column(children: [const Text("Bidan Setting")]);
+  }
+
+  Widget _adminSettings() {
+    return Column(children: [const Text("Admin Setting")]);
   }
 }
